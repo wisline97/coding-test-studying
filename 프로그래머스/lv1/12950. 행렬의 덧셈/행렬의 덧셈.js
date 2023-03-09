b51cc2f62cdf3a2249cc384e648c2e14a2156a04
Jisun <105546706+wisline97@users.noreply.github.com>
2023-03-09 11:04:23 +0900

    [level 1] Title: 행렬의 덧셈, Time: 6.44 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,9 @@
+function solution(arr1, arr2) {
+    var answer = arr1
+    for(i=0; i<arr1.length; i++){
+        for(j=0; j<arr1[i].length; j++){
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+        }
+    }
+    return answer;
+}
