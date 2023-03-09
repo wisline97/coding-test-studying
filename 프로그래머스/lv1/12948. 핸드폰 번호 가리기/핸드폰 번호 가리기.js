6017da71f03779faef3215ca5dd450cbda6c1ee1
Jisun <105546706+wisline97@users.noreply.github.com>
2023-03-09 11:04:34 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,8 @@
+function solution(phone_number) {
+    var answer = '';
+    for(i=0; i<phone_number.length-4; i++){
+        answer += '*'
+    }
+    answer = answer + phone_number.substring(phone_number.length-4,phone_number.length)
+    return answer;
+}
