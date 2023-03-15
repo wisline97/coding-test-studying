46bf1b8ff82aae882dfe982142ee28523e948bf8
Jisun <105546706+wisline97@users.noreply.github.com>
2023-03-15 11:38:54 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -4,9 +4,5 @@ number = int(sys.stdin.readline())
 arr = list(map(int,sys.stdin.readline().split()))
 
 v = int(sys.stdin.readline())
-count = 0
 
-for i in range(number):
-    if arr[i] == v:
-        count += 1
-print(count)
+print(arr.count(v))
