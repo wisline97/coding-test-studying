2c3a4116e128c8eec96c9c1181d19dbeb2ecd379
Jisun <105546706+wisline97@users.noreply.github.com>
2023-03-15 11:44:38 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32408 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,9 @@
+import sys
+
+n, x = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+
+for i in range(n):
+    if a[i] < x:
+        print(a[i], end=" ")
+print()
