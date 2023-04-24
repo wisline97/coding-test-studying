8701f7c84e7ccc4f6c30b6dab15ad109355aa2b6
Jisun <105546706+wisline97@users.noreply.github.com>
2023-04-24 21:07:05 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,20 @@
+n = int(input())
+star = 1
+
+check = False
+for i in range(n*2 - 1):
+    blank = (n*2) - (star*2)
+    for s in range(star):
+        print("*",end="")
+    for b in range(blank):
+        print(" ",end="")
+    for s in range(star):
+        print("*",end="")
+    if star == n:
+        check = True
+    if not check:
+        star += 1
+    elif check:
+        star -= 1
+    
+    print()
